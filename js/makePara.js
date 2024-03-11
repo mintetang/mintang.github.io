56802902ce373be2e219fd949a237988052fd141
Joseph Tang <162398761+mintetang@users.noreply.github.com>
2024-03-11 15:37:05 +0800

    Update makePara.js

--- js/makePara.js
+++ js/makePara.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const buttons = document.querySelectorAll("button");
 
-  for (const button of buttons) {
-    button.addEventListener("click", createParagraph);
+  for (const button2 of buttons) {
+    button2.addEventListener("click", createParagraph);
   }
 });
